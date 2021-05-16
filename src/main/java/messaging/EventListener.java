d396fc610559d2a0709e4b92867e5c653e9183f3
cwenling <e0425906@u.nus.edu>
2021-05-17 00:29:42 +0800

    Add classes for messaging layer

---
+++ src/main/java/messaging/EventListener.java
@@ -0,0 +1,10 @@
+package messaging;
+
+import com.binance.api.client.domain.market.OrderBook;
+
+public class EventListener {
+    void handleEvent(OrderBook orderBook) {
+    }
+    void handleEvent(ScheduleEvent timer) {
+    }
+}
---
+++ src/main/java/messaging/EventManager.java
@@ -0,0 +1,10 @@
+package messaging;
+
+import com.binance.api.client.domain.market.OrderBook;
+
+public class EventManager {
+    void publish(OrderBook orderBook) {
+    }
+    void publish(ScheduleEvent timer) {
+    }
+}
