41ce5a0756082d027d56bfa113a62e76b8e6d43f
cwenling <e0425906@u.nus.edu>
2021-05-18 23:47:12 +0800

    Modify EventManager

--- src/main/java/messaging/EventManager.java
+++ src/main/java/messaging/EventManager.java
@@ -3,7 +3,35 @@ package messaging;
 import com.binance.api.client.domain.market.OrderBook;
 import source.ScheduleEvent;
 
-public interface EventManager {
-    void publish(OrderBook orderBook);
-    void publish(ScheduleEvent timer);
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class EventManager {
+    private BlockingQueue<OrderBook> orderBooks;
+    private List<EventListener> listeners;
+    public EventManager() {
+        this.orderBooks = new ArrayBlockingQueue<OrderBook>(100);
+        this.listeners = new ArrayList<EventListener>();
+    }
+    void publish(OrderBook orderBook) {
+        orderBooks.add(orderBook);
+    }
+
+    void publish(ScheduleEvent timer) {
+
+    }
+
+    void addListener(EventListener listener) {
+        if (!listeners.contains(listener)) {
+            listeners.add(listener);
+        }
+    }
+
+    void removeListener(EventListener listener) {
+        if (listeners.contains(listener)) {
+            listeners.remove(listener);
+        }
+    }
 }
