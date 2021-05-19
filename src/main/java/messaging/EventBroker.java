26e62c8461e6a84aed4a79b48a2ab73182c786da
cwenling <e0425906@u.nus.edu>
2021-05-20 00:12:13 +0800

    Add EventBroker class

---
+++ src/main/java/messaging/EventBroker.java
@@ -0,0 +1,52 @@
+package messaging;
+
+import source.LocalOrderBook;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class EventBroker<T> {
+    private BlockingQueue<T> eventQueue = new ArrayBlockingQueue<T>(1024);
+    private List<EventListener> listenerList = new ArrayList<>();
+
+    public void addEvent(T event) throws InterruptedException {
+        eventQueue.put(event);
+    }
+
+    public void broadcast() throws InterruptedException {
+        if (eventQueue.isEmpty()) {
+            System.out.println("No events to broadcast.");
+        } else {
+            while (!eventQueue.isEmpty()) {
+                T event = eventQueue.remove();
+                sendToListeners(event);
+            }
+        }
+    }
+
+    public void sendToListeners(T event) throws InterruptedException {
+        for (EventListener listener : listenerList) {
+            if (event instanceof LocalOrderBook) {
+                listener.handleEvent((LocalOrderBook) event);
+            }
+        }
+    }
+
+    public void addListener(EventListener listener) {
+        if (!listenerList.contains(listener)) {
+            listenerList.add(listener);
+        } else {
+            System.out.println("Listener already exists in list.");
+        }
+    }
+
+    public void removeListener(EventListener listener) {
+        if (listenerList.contains(listener)) {
+            listenerList.remove(listener);
+        } else {
+            System.out.println("Listener does not exist in list");
+        }
+    }
+}
